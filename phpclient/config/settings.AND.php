f7deed3b56b7f61ec771ef2bb51b1a5f7cce1421
javaskater <pythonrubylang@gmail.com>
2016-12-07 23:42:15 +0100

    ajout d'une configuration pour tests 1AND1

---
+++ phpclient/config/settings.AND.php
@@ -0,0 +1,20 @@
+<?php
+
+$mysql_settings = [
+    'host' => 'db659653007.db.1and1.com',
+    'port' => '3306',
+    'name' => 'db659653007',
+    'username' => 'dbo659653007',
+    'password' => 'scala39Skater',
+    'charset' => 'utf8'
+];
+
+$chemins_fichiers = [
+    'repertoire_csv' => '/kunden/homepages/21/d462702613/htdocs/livrables_jpm/importations',
+    'repertoire_log' => '/kunden/homepages/21/d462702613/htdocs/livrables_jpm/importations',
+];
+
+$other_settings = [
+    'log_history_depth' => 2 //nombre de fichiers logs à garder
+];
+
---
+++ phpclient/config/settings.LOCAL.php
@@ -0,0 +1,20 @@
+<?php
+
+$mysql_settings = [
+    'host' => '127.0.0.1',
+    'port' => '3306',
+    'name' => 'rif',
+    'username' => 'rif',
+    'password' => 'rif',
+    'charset' => 'utf8'
+];
+
+$chemins_fichiers = [
+    'repertoire_csv' => '/home/jpmena/RIF/importations',
+    'repertoire_log' => '/home/jpmena/RIF',
+];
+
+$other_settings = [
+    'log_history_depth' => 20 //nombre de fichiers logs à garder
+];
+
